08f9ed6cd945050d041a5317d2a83a1e8faec1d1
Samuel Martins <samuel.mrtins2016@gmail.com>
2023-12-01 15:09:07 -0300

    cardPagJogos

--- GameAcademyReactTS/src/pages/Teste/index.tsx
+++ GameAcademyReactTS/src/pages/Teste/index.tsx
@@ -1,9 +1,11 @@
+import CardPagJogos from "../../components/CardPagJogos";
 import Header from "../../components/Header";
 
 export default function Teste() {
     return (
         <>
             <Header />
+            <CardPagJogos />
         </>
     )
 }
