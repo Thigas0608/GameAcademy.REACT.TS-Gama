db49f4a6f25515deb9b4f2c915e09a89d594898a
Thiago Henrique <th0608thiago@gmail.com>
2023-12-02 01:03:32 -0300

    Structuring all pages

---
+++ src/pages/Players/index.tsx
@@ -0,0 +1,17 @@
+import { Footer } from "../../components/Footer";
+import SideBar from "../../components/SideBar";
+import { ContainerBody, ContainerConteudo } from "../Games/style";
+ 
+export default function Players() {
+    return (
+        <>
+            <ContainerBody>
+                <SideBar />
+                <ContainerConteudo>
+                   
+                </ContainerConteudo>
+            </ContainerBody>
+            <Footer />
+        </>
+    );
+}
--- src/pages/Ranking/index.tsx
+++ src/pages/Ranking/index.tsx
@@ -1,7 +1,17 @@
+import { Footer } from "../../components/Footer";
+import SideBar from "../../components/SideBar";
+import { ContainerBody, ContainerConteudo } from "../Games/style";
+ 
 export default function Ranking() {
     return (
         <>
-            <h1>Ranking</h1>
+            <ContainerBody>
+                <SideBar />
+                <ContainerConteudo>
+                   
+                </ContainerConteudo>
+            </ContainerBody>
+            <Footer />
         </>
     );
 }
