60caabd7abff6df07de9f35292710601d3f66a64
Samuel Martins <samuel.mrtins2016@gmail.com>
2023-12-03 15:26:40 -0300

    Integração Ranking Geral

--- src/pages/Ranking/index.tsx
+++ src/pages/Ranking/index.tsx
@@ -2,17 +2,61 @@ import { Footer } from "../../components/Footer";
 import SideBar from "../../components/SideBar";
 import { ContainerBody, ContainerConteudo } from "./style";
 import { Title } from "../../components/Titles/style";
+import RankingTabela from "../../components/RankingTabela";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+interface Jogador {
+  id: number;
+  nome: string;
+  email: string;
+  nickname: string;
+  imagem: string;
+}
+
+interface Jogo {
+  id: number;
+  nome: string;
+  autor: string;
+  website: string;
+  imagem: string;
+  descricao: string;
+}
+
+interface Pontuacao {
+  id: number;
+  pontos: number;
+  createAt: number;
+  jogador: Jogador;
+  jogo: Jogo;
+}
 
 export default function Ranking() {
-    return (
-        <>
-            <ContainerBody>
-                <SideBar />
-                <ContainerConteudo>
-                    <Title> RANKING </Title>
-                </ContainerConteudo>
-            </ContainerBody>
-            <Footer />
-        </>
-    );
+  const [pontuacoes, setPontuacoes] = useState<Pontuacao[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<Pontuacao[]>("http://localhost:8080/ranking");
+        setPontuacoes(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar pontuações:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <ContainerBody>
+        <SideBar />
+        <ContainerConteudo>
+          <Title> RANKING </Title>
+          <RankingTabela />
+        </ContainerConteudo>
+      </ContainerBody>
+      <Footer />
+    </>
+  );
 }
